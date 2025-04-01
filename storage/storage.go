2e9ff0462b3bf26fd2df5f595cb01ca0aefc1fe1
Sergio Sanchez <ssantiago065@gmail.com>
2025-03-31 23:06:20 -0600

    feat: add functions to load and save tasks to JSON file

--- storage/storage.go
+++ storage/storage.go
@@ -1,1 +1,44 @@
 package storage
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/ssantiago065/task_tracker/tasks"
+)
+
+func LoadTasks(filename string) ([]tasks.Task, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		if os.IsNotExist(err) { // If file does not exist, return empty list
+			return []tasks.Task{}, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	var tasks []tasks.Task
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&tasks)
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func SaveTasks(filename string, tasks []tasks.Task) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(tasks)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
